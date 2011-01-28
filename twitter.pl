22c68ada5bb7c78b3d408f94e762da1a50fd4ebf
"Loren M. Lang" <lorenl@north-winds.org>
2011-01-27 19:38:16 -0800

    Added support for OAuth and multi-source to twitter.pl.
    Twitter.pl still uses RSS api, but can now access private feeds and have multiple feeds configured.

--- twitter.pl
+++ twitter.pl
@@ -37,27 +37,109 @@ use open ':utf8', ':std';
 use FindBin;
 use lib "$FindBin::Bin", "$FindBin::Bin/lib";
 use Getopt::Long;
+use LWP::UserAgent;
+use HTTP::Headers;
+use HTTP::Request;
+use HTTP::Request::Common;
 
 require 'common.pl';
 
+my $id;
 my $verbose = 0;
-my $result = GetOptions("verbose" => \$verbose);
+my $result = GetOptions(
+	"id=i" => \$id,
+	"verbose" => \$verbose);
+die "Usage: $0 [-id id] [-verbose] [twitter.xml]" if !$result || @ARGV > 1;
 
 my $self = init();
 $self->{verbose} = $verbose;
 lockKml($self);
 
-my $rssFile = "twitter.rss";
-$rssFile = shift if @ARGV;
+my $source;
+my $twitterFile = $self->{settings}->{twitterFeed};
+$twitterFile = shift if @ARGV;
+
+if(defined($twitterFile)) {
+	$source = {
+		id => 12,
+		name => "Twitter",
+		deviceKey => 12,
+		file => $twitterFile
+	};
+} else {
+	$source = findSource($self, "Twitter", $id);
+	$twitterFile = $source->{file};
+	$twitterFile = 'http://api.twitter.com/1/statuses/user_timeline.rss'
+	    if !defined($twitterFile);
+}
+
+# This is a wrapper for LibXML allowing it to pull from URLs as well as
+# from files.  This was shamelessly stolen from XML::DOM.
+sub loadXML {
+	my($url) = @_;
+	my $parser = new XML::LibXML;
+
+	# Any other URL schemes?
+	if($url =~ /^(https?|ftp|wais|gopher|file):/) {
+		# Read the file from the web with LWP.
+		#
+		# Note that we read in the entire file, which may not be ideal
+		# for large files. LWP::UserAgent also provides a callback style
+		# request, which we could convert to a stream with a fork()...
+
+		my $result;
+		eval {
+			use LWP::UserAgent;
+
+			my $ua = LWP::UserAgent->new;
+
+			my $oauthData = {
+				protocol => 'oauth 1.0a',
+				app => 'Twitter',
+				type => 'Resource',
+				params => {
+					token => $source->{token},
+					token_secret => $source->{tokenSecret},
+					request_url => $url,
+					request_method => 'GET',
+				},
+			};
+
+			my $headers = new HTTP::Headers;
+			$headers->header('Authorization', requestSign($oauthData));
+			# Load proxy settings from environment variables, i.e.:
+			# http_proxy, ftp_proxy, no_proxy etc. (see LWP::UserAgent(3))
+			# You need these to go thru firewalls.
+			$ua->env_proxy;
+			my $req = new HTTP::Request 'GET', $url, $headers;
+			#print Dumper($req);
+			my $response = $ua->request($req);
+			die "Bad request ".$response->status_line if !$response->is_success;
+
+			# Parse the result of the HTTP request
+			$result = $parser->parse_string($response->content);
+		};
+		if($@) {
+			die "Couldn't parsefile [$url] with LWP: $@";
+		}
+		return $result;
+	} else {
+		return $parser->parse_file($url);
+	}
+}
 
 my $doc = loadKml($self);
 my $xc = loadXPath($self);
-my @base = $xc->findnodes("/kml:kml/kml:Document/kml:Folder[kml:name='Twitter']", $doc);
-my $parser = new XML::LibXML;
-my $rssDoc = $parser->parse_file($rssFile);
-my @items = $xc->findnodes('/rss/channel/item', $rssDoc);
+my $containerPath = "/kml:kml/kml:Document/kml:Folder[kml:name='Twitter']";
+my $containerId = $source->{kml}->{container};
+$containerPath = "//kml:Folder[\@id='$containerId']" if defined($containerId);
+my @base = $xc->findnodes($containerPath, $doc);
+#my $parser = new XML::LibXML;
+#my $twitterDoc = $parser->parse_file($twitterFile);
+my $twitterDoc = loadXML($twitterFile);
+my @items = $xc->findnodes('/rss/channel/item', $twitterDoc);
 
-die "Can't find base for twitter" if @base != 1;
+die "Can't find container for Twitter" if @base != 1;
 
 #print "List:\n";
 foreach my $item (reverse @items) {
@@ -95,4 +177,8 @@ foreach my $item (reverse @items) {
 	addPoint($doc, $mark, $entry->{latitude}, $entry->{longitude}, $entry->{altitude});
 	addPlacemark($doc, $base[0], $mark);
 }
+
+print "Saving location data.\n" if $self->{verbose};
+appendData($self, $newEntries);
+
 saveKml($self, $doc);
